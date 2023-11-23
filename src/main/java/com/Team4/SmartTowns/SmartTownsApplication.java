46dafb37c38deef0e936b3446e4a10d308f01808
James Pang <Pangjy@cardiff.ac.uk>
2023-11-23 12:19:29 +0000

    Added Springboot Server Build Files

---
+++ src/main/java/com/Team4/SmartTowns/SmartTownsApplication.java
@@ -0,0 +1,13 @@
+package com.Team4.SmartTowns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTownsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTownsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Team4/SmartTowns/SmartTownsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Team4.SmartTowns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTownsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
