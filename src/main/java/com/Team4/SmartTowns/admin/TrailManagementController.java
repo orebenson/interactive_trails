3d397e9c325730f9710c0e42e0910da5013cebe3
Ore Benson <bensono1@cardiff.ac.uk>
2023-11-27 14:48:26 +0000

    Added submission of new trails form

--- src/main/java/com/Team4/SmartTowns/admin/TrailManagementController.java
+++ src/main/java/com/Team4/SmartTowns/admin/TrailManagementController.java
@@ -1,8 +1,13 @@
 package com.Team4.SmartTowns.admin;
 
+import com.Team4.SmartTowns.trails.Trail;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,6 +15,21 @@ public class TrailManagementController {
     @GetMapping("/trails/add")
     public ModelAndView addTrail(){
         ModelAndView mav = new ModelAndView("admin/createTrail");
+        mav.addObject("trail", new Trail());
         return mav;
     }
+
+
+    @PostMapping("/trails/add")
+    public ModelAndView submitTrail(@ModelAttribute("trail") Trail trail){
+        // use the trail service to add the trail
+        // if no errors:
+            // trailService.createTrail(trail)
+        ModelAndView mav = new ModelAndView("redirect:/");
+        return mav;
+    }
+
+
+//
+//    @PostMapping("/trails/{id}/delete")
 }
