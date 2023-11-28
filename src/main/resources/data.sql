c34bca2d6d4d2dcd8b0aacdb2d51bf1064e095f3
Ore Benson <bensono1@cardiff.ac.uk>
2023-11-28 18:46:23 +0000

    Added login/logout functionality, with default admin account

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,16 @@
 insert into roles_table (role_id, name)
-values
-    (1, 'ADMIN'),
-    (2, 'USER');
+values (1, 'ADMIN'), (2, 'USER');
 
-# insert into user_table (username, password)
-# values
-#     ('admin', '$2a$12$zKMHlbOC7UXOLCdq5ZibC.ANCzcxgpoGbode97Dc2Fi1zakG2fP6O');
-#
-# insert into users_roles (username, role_id)
-# values
-#     ('admin', 1);
+-- add default admin account
+insert into user_table (username, password, enabled)
+values ('admin', '$2a$12$zKMHlbOC7UXOLCdq5ZibC.ANCzcxgpoGbode97Dc2Fi1zakG2fP6O', true);
 
+insert into users_roles (username, role_id)
+values ('admin', 1);
+
+-- add a default user
+insert into user_table (username, password, enabled)
+values ('test', '$2a$12$NLq52ZFttP9BUQ78oKxjwuX/yED0HFV0eQFDvWzbwEgMhBqNk7rSG', true);
+
+insert into users_roles (username, role_id)
+values ('user', 2);
