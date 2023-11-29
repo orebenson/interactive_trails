0e2db5c80cbe5b4c67c6bdbb238f53c15f68ceba
James Pang <Pangjy@cardiff.ac.uk>
2023-11-29 14:32:31 +0000

    added default temporary trails and checkpoints, and added relevant methods

--- src/main/java/com/Team4/SmartTowns/trails/model/TrailController.java
+++ src/main/java/com/Team4/SmartTowns/trails/model/TrailController.java
@@ -15,51 +15,64 @@ import java.util.List;
 public class TrailController {
 
     private TrailService trailService;
-
-    public TrailController(TrailService trailService){
-
-        this.trailService = trailService;
-//        Trail tempTrail = new Trail();
-//        tempTrail.setName();
-//        tempTrail.setLocation();
-//        tempTrail.setDescription();
-//        tempTrail.getCheckpoints().get(0).setDescription()
-//        tempTrail.getCheckpoints().get(0).getCoordinates()[0] = 13253.13535;
-//        tempTrail.getCheckpoints().get(0).getCoordinates()[0] = 13253.13535;
-//        trailService.createTrail();
-    }
-
     private CheckpointService checkpointService;
 
-    public void CheckpointService(CheckpointService checkpointService) {
-    this.checkpointService = checkpointService;
+    public TrailController(TrailService trailService, CheckpointService checkpointService){
+
+        this.trailService = trailService;
+        this.checkpointService = checkpointService;
+
+        // Create some default trails
+        Trail trail1 = createTempTrail("Cardiff Castle to Queen Street", "Cardiff", "A serene town along the shore, blending cobblestone streets with beachside cafes and historic cottages.");
+        Trail trail2 = createTempTrail("Newport Castle to the Transporter Bridge", "Newport","Newport, a historic town, showcases colonial charm with cobbled streets and well-preserved architecture. Situated along the river, it reflects a blend of heritage and modernity, making it a captivating destination in Wales.");
+        Trail trail3 = createTempTrail("Swansea Mumbles to Gower", "Swansea","Nestled along the scenic coastline, Swansea is a coastal haven known for its quaint beauty. With sandy beaches, a bustling marina, and a rich maritime history, the town offers a serene escape by the sea.");
+
+        createTempCheckpoint(trail1,0, "Cardiff Castle", "A historic fortress in Cardiff, Wales, blending Roman, medieval, and Victorian architecture in the heart of the city.");
+        createTempCheckpoint(trail1,1, "checkpoint 2", "(Description of checkpoint 2)");
+        createTempCheckpoint(trail2,0, "Newport Castle", "(Description of checkpoint 1)");
+        createTempCheckpoint(trail3,0, "Mumbles", "(Description of checkpoint 1)");
+
+        trailService.createTrail(trail1);
+        trailService.createTrail(trail2);
+        trailService.createTrail(trail3);
+
+    }
+
+    private Trail createTempTrail(String name, String location, String description) {
+        // Method for helping create default trails
+        Trail tempTrail = new Trail();
+        tempTrail.setName(name);
+        tempTrail.setLocation(location);
+        tempTrail.setDescription(description);
+        return tempTrail;
+    }
+
+    private Trail createTempCheckpoint(Trail trail, int pos, String name, String description) {
+        // Method to help create default trails checkpoint
+        List<Checkpoint> checkpoints = trail.getCheckpoints();
+        Checkpoint temp = checkpoints.get(pos);
+        temp.setName(name);
+        temp.setDescription(description);
+        trail.setCheckpoints(checkpoints);
+        return trail;
     }
 
     @GetMapping("/trails")
     public ModelAndView showTrails() {
         ModelAndView mvc = new ModelAndView("/trails/trails");
         List<Trail> trails = trailService.getAllTrails();
-
         mvc.addObject("trails", trails);
 
         return mvc;
-
-        //adding some default data to populate the page.
-//        mvc.addObject("id", "Trail ID: 1");
-//        mvc.addObject("name", "Trail Name: Cardiff Castle to Queen Street");
-//        mvc.addObject("location", "Location: Cardiff");
-//        mvc.addObject("description", "Description: Cardiff Castle to Queen Street description");
-
-
     }
 
     @GetMapping("/trails/{id}")
-    public ModelAndView getDetails(@PathVariable Long id, HttpSession session){
+    public ModelAndView getDetails(@PathVariable Long id){
         ModelAndView mvc = new ModelAndView("/trails/trailDetails");
-        Trail trails = trailService.getTrailById(id);
+        Trail trail = trailService.getTrailById(id);
 //        List<Checkpoint> checkpoints = checkpointService.getAllCheckpoints();
 //        mvc.addObject("checkpoints",checkpoints);
-        mvc.addObject("trail", trails);
+        mvc.addObject("trail", trail);
         return mvc;
     }
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,7 @@
-insert into roles_table (name)
-values
-    ('ADMIN'),
-    ('USER');
-
-insert into trail_table (name, location, description)
-values ('Cardiff Castle to Queen Street', 'Cardiff', 'A serene town along the shore, blending cobblestone streets with beachside cafes and historic cottages.');
-insert into trail_table (name, location, description)
-values ('Newport Castle to the Transporter Bridge', 'Newport','Newport, a historic town, showcases colonial charm with cobbled streets and well-preserved architecture. Situated along the river, it reflects a blend of heritage and modernity, making it a captivating destination in Wales.');
-insert into trail_table (name, location, description)
-values ('Swansea Mumbles to Gower', 'Swansea','Nestled along the scenic coastline, Swansea is a coastal haven known for its quaint beauty. With sandy beaches, a bustling marina, and a rich maritime history, the town offers a serene escape by the sea.');
-
 insert into roles_table (role_id, name)
-values (1, 'ADMIN'), (2, 'USER');
+values
+    (1, 'ADMIN'),
+    (2, 'USER');
 
 -- add default admin account
 insert into user_table (username, password, enabled)
