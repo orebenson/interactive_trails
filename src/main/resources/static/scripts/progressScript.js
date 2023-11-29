b2b9b5c37d2ded69bf289109b210d30e4e29b9c4
Ayman Pasha <PashaAJ@cardiff.ac.uk>
2023-11-29 15:49:18 +0000

    progression bar

--- src/main/resources/static/scripts/progressScript.js
+++ src/main/resources/static/scripts/progressScript.js
@@ -1,9 +1,32 @@
 function newProgress(){
-    var newValue = document.getElementById("completed").value;
-    var progressBar = document.getElementById("progressBar");
-    var progression = Math.min(Math.max(parseInt(newValue), 0), 100);
+    let newValue = document.getElementById("completed").value;
+    let workingProgressBar = document.getElementById("progressBar");
+    let progression = Math.min(Math.max(parseInt(newValue), 0), 100);
+    let titleStatus = document.getElementById("status");
+    let newStoredProgress = 0;
 
-    
-    progressBar.style.width = progression + "%";
-    progressBar.innerHTML = progression + "%";
+    workingProgressBar.style.width = progression + "%";
+    workingProgressBar.innerHTML = progression + "%";
+
+    if(progression >= 100){
+        workingProgressBar.innerHTML = "Trail Complete!"
+        titleStatus.innerHTML = "Status: Completed!"
+    }
+    if(progression <=99){
+        titleStatus.innerHTML = "Status: Incomplete"
+    }
+
+
+}
+
+function storedProgress(value){
+    newStoredProgress = newValue;
+    newProgress(newValue);
+}
+
+function completedTrail(){
+    function completeTrailTitle(){
+
+
+    }
 }
