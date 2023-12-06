00b2a670632d0dc4f9a7a512550d979838cc6c48
Ore Benson <bensono1@cardiff.ac.uk>
2023-12-06 13:51:09 +0000

    Added sorting functionality

--- src/main/resources/static/scripts/leaderboardScripts.js
+++ src/main/resources/static/scripts/leaderboardScripts.js
@@ -17,3 +17,37 @@ function filterTable() { // filter the table by username, using search bar
         }
     }
 }
+
+function sortTable() {
+    const table = document.getElementById("leaderboardTable");
+    const tbody = table.getElementsByTagName("tbody")[0];
+    const rows = Array.from(tbody.getElementsByTagName("tr"));
+
+    const sortOption = document.getElementById("sort").value;
+
+    const compareFunction = (a, b) => {
+        if (sortOption === "username") {
+            const usernameA = a.getElementsByTagName("td")[1].innerText.toLowerCase();
+            const usernameB = b.getElementsByTagName("td")[1].innerText.toLowerCase();
+            return usernameA.localeCompare(usernameB);
+        } else if (sortOption === "position") {
+            const positionA = parseInt(a.getElementsByTagName("td")[0].innerText);
+            const positionB = parseInt(b.getElementsByTagName("td")[0].innerText);
+            return positionA - positionB;
+        }
+        return 0;
+    };
+
+    const sortedRows = rows.sort(compareFunction);
+
+    // Remove existing rows
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
+
+    // Append sorted rows to the tbody
+    sortedRows.forEach(row => {
+        tbody.appendChild(row);
+    });
+}
+
