b3a89ffc6bed18efa4442f5ce093100644fca543
Ore Benson <bensono1@cardiff.ac.uk>
2023-12-09 19:54:57 +0000

    bug fixes

--- src/main/java/com/Team4/SmartTowns/progression/service/progressionService.java
+++
@@ -1,9 +0,0 @@
-package com.Team4.SmartTowns.progression.service;
-import com.Team4.SmartTowns.progression.model.progressionUser;
-import java.util.List;
-
-public interface progressionService {
-    List<progressionUser> getProgress();
-
-
-}
